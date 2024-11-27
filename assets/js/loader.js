964c8113944a50d69b7014b944bd5322ad7cde63
Maheswaran2512 <devamahesh8116@gmail.com>
2024-11-27 22:39:45 +0530

    load

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,6 +1,6 @@
 // Wait for the page to fully load
 window.addEventListener('load', function () {
     const loader = document.getElementById('loader');
-    loader.style.display = 'none'; // Hide loader after the page loads
+    loader.classList.add('hidden');
+    setTimeout(() => loader.style.display = 'none', 500); // Matches transition duration
   });
-  
